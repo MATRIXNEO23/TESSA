b8c16f387bf81d0e271dbd576469a3fd0e946865
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-17 13:45:16 +0200

    Add WebView activity for correspondence console

---
+++ android-apk/app/src/main/java/io/github/matrixneo23/tessachat/MainActivity.java
@@ -0,0 +1,24 @@
+package io.github.matrixneo23.tessachat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://matrixneo23.github.io/TESSA/chat/");
+    }
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
